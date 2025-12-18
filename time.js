08b66928bcdb1929ee3c590d5fd2462d7735b3e7
sarangoo.github.io <ganboldsarangoo55@gmail.com>
2025-12-19 02:37:07 +0800

    Delete time.js

--- time.js
+++
@@ -1,8 +0,0 @@
-let n = parseFloat(prompt("Тоо оруулна уу:"));
-let hour = new Date().getHours();
-
-if (hour >= 6 && hour < 12) {
-    console.log("Өглөө байна. 2 зэрэг:", Math.pow(n, 2));
-} else {
-    console.log("Орой байна. Язгуур:", Math.sqrt(n).toFixed(2));
-}
